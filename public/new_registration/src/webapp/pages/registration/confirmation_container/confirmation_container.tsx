c3bf735203458b1e11587ef40c3a261eb998fbe9
JohanHjalmarsson <johan.g.hjalmarsson@gmail.com>
2019-04-16 12:46:21 +0200

    confirm page with message

--- public/new_registration/src/webapp/pages/registration/confirmation_container/confirmation_container.tsx
+++ public/new_registration/src/webapp/pages/registration/confirmation_container/confirmation_container.tsx
@@ -1,9 +1,23 @@
 import * as React from 'react'
+import { GENERAL_TEXT } from 'src/webapp/config/text';
+import styles from './style'
 
 class ConfirmationContainer extends React.Component {
     render() {
         return (
-            <div>bekräfta</div>
+            <div style={styles.container}>
+                <div style={styles.header}>
+                    <h1>Din anmälan är klar!</h1>
+                </div>
+                <div style={styles.textContainer}>
+                    <p>{GENERAL_TEXT.after_submit}</p>
+                    
+                </div>
+                <div style={styles.footer}>
+                    <a href="http://www.designpriset.se">Tillbaka till designpriset.se</a>
+                </div>
+
+            </div>
         )
     }
 }
---
+++ public/new_registration/src/webapp/pages/registration/confirmation_container/style.ts
@@ -0,0 +1,17 @@
+import { PRIMARY_BACKGROUND_COLOR } from 'src/webapp/config/style';
+
+export default {
+    container: {
+        backgroundColor: PRIMARY_BACKGROUND_COLOR,
+        height: '100vh'
+    },
+    header: {
+        textAlign: 'center' as 'center'
+    },
+    textContainer: {
+        padding: 30,
+    },
+    footer: {
+        textAlign: 'center' as 'center'
+    }
+}
