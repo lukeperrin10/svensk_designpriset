8431773bcb9f3109e6603c879640288454e3dd26
Hjalmar <hjalmar@wopii.com>
2019-11-22 15:28:07 +0100

    Started config admin

--- backend/admin/despri_admin/admin.py
+++ backend/admin/despri_admin/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Category, Entry, Poll, Profile, Vote
+from .models import Category, Entry, Poll, Profile, Vote, Phase, YearConfig
+
+
+
+
 
 
 admin.site.register(Category)
@@ -7,3 +11,5 @@ admin.site.register(Entry)
 admin.site.register(Poll)
 admin.site.register(Profile)
 admin.site.register(Vote)
+#admin.site.register(Phase, PhaseAdmin)
+admin.site.register(YearConfig)
