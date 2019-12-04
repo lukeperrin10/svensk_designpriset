40ef05ef0730c4affebaeeaf579558cb1edf37be
JohanHjalmarsson <johan.g.hjalmarsson@gmail.com>
2019-12-04 11:19:33 +0100

    mail vars widget in add mail admin

--- backend/admin/despri_admin/admin.py
+++ backend/admin/despri_admin/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.utils.html import format_html
 from django.utils.translation import ugettext_lazy as _
-from .models import Category, Entry, Poll, Profile, Vote, Phase, YearConfig, Content, Mail
+from .models import Category, Entry, Poll, Profile, Vote, Phase, YearConfig, Content, Mail, MailVar
 from .admins.entry_admin import EntryAdmin
 from .admins.base_admin import BaseAdmin
 from .admins.poll_admin import PollAdmin
+from .admins.mail_admin import MailAdmin
 
 
 
@@ -18,4 +19,5 @@ admin.site.register(Vote)
 #admin.site.register(Phase, PhaseAdmin)
 admin.site.register(YearConfig, BaseAdmin)
 admin.site.register(Content, BaseAdmin)
-admin.site.register(Mail, BaseAdmin)
+admin.site.register(Mail, MailAdmin)
+admin.site.register(MailVar, BaseAdmin)
---
+++ backend/admin/despri_admin/admins/mail_admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+from .base_admin import BaseAdmin
+from ..models import MailVar
+
+class MailAdmin(BaseAdmin):
+    change_form_template = 'admin/despri_admin/mail_change_form.html'
+    
+    def get_mail_vars(self):
+        mailVars = MailVar.objects.all()
+        return mailVars
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['vars'] = self.get_mail_vars()
+        return super(MailAdmin, self).change_view(
+            request, object_id, form_url, extra_context=extra_context,
+        )
+
+    def add_view(self, request, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['vars'] = self.get_mail_vars()
+        return super(MailAdmin, self).add_view(
+            request, form_url, extra_context=extra_context,
+        )
