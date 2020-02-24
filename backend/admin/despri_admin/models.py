b8ccfa2f9abab36776f961e326c73f38be3f4293
Hjalmar <hjalmar@wopii.com>
2020-02-24 12:30:08 +0100

    Checkbox for active categories

--- backend/admin/despri_admin/models.py
+++ backend/admin/despri_admin/models.py
@@ -20,6 +20,7 @@ class Category(BaseModel):
     name = models.CharField(_('Name'), max_length=255, null=False)
     description = models.TextField(_('Description'), null=True, blank=True)
     shorttag = models.CharField(_('Shorttag'), max_length=20, null=False,unique=True)
+    active = models.BooleanField(_('Active'), null=False, blank=True, default=True)
 
     def __str__(self):
         return self.name
