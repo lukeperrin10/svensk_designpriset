fd5bbb0a88c650bc57a8b29df15bfcc87b0fd16c
JohanHjalmarsson <johan.g.hjalmarsson@gmail.com>
2020-03-05 11:26:31 +0100

    add preview in content

--- backend/admin/despri_admin/admins/content_admin.py
+++ backend/admin/despri_admin/admins/content_admin.py
@@ -1,6 +1,14 @@
 from django_summernote.admin import SummernoteModelAdmin
+from django.utils.html import format_html
 from .base_admin import BaseAdmin
 from ..models import Content
 
 class ContentAdmin(SummernoteModelAdmin, BaseAdmin):
     summernote_fields = ('content',)
+    readonly_fields = ('image_preview','preview')
+    
+    def preview(self, obj):
+        return format_html(format(obj.content))
+
+    def image_preview(self, obj):
+        return format_html('<a href="{0}"><img width="700px" src="{0}" /></a>'.format(obj.image.url))
