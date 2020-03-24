43fef6e569c62ac04f0bef4105a2092e150c75cd
JohanHjalmarsson <johan.g.hjalmarsson@gmail.com>
2020-03-24 09:45:00 +0100

    style standard page

--- public/new_registration/src/webapp/components/standard_page/standard_page.tsx
+++ public/new_registration/src/webapp/components/standard_page/standard_page.tsx
@@ -4,6 +4,8 @@ import {Helmet} from 'react-helmet'
 import Meta from '../meta'
 import { assembleMediaUrl } from '../../helpers'
 import PageContainer from '../page_container'
+import Text from '../text'
+import { TEXT_TYPES, HEADLINE_SIZES } from '../text/text'
 
 interface props {
     title: string
@@ -18,9 +20,11 @@ const StandardPage = ({title, content, image}:props) => {
             title={title}
             image={image} />
             <article>
-                <h2>{title}</h2>
-                <img src={assembleMediaUrl(image)} alt={`${title}_image`} />
-                <div dangerouslySetInnerHTML={{__html: content}} />
+                <img className={styles.img} src={assembleMediaUrl(image)} alt={`${title}_image`} />
+                <Text className={styles.header} type={TEXT_TYPES.H1} headlineSize={HEADLINE_SIZES.LARGE}>
+                    {title}
+                </Text>
+                <div className={styles.content} dangerouslySetInnerHTML={{__html: content}} />
             </article>
         </PageContainer>
     )
